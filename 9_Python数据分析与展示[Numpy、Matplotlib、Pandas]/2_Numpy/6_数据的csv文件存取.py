03c4be35f9840b6388639a2156db7a488dc3efc3
feilong <1634025627@qq.com>
2019-12-11 16:40:02 +0800

    提交

--- 9_Python数据分析与展示[Numpy、Matplotlib、Pandas]/2_Numpy/6_数据的csv文件存取.py
+++ 9_Python数据分析与展示[Numpy、Matplotlib、Pandas]/2_Numpy/6_数据的csv文件存取.py
@@ -1,4 +1,4 @@
-# 1、Numpy向csv文件保存函数savetx()
+# 1、Numpy向csv文件写入数据，函数savetx()
 """
     numpy.savetxt(fraem,array,fmt='%.18e',delimiter=None)
     frame   文件、字符串或产生器，可以是.gz或.bz2的压缩文件
@@ -7,7 +7,20 @@
     delimiter   分割字符串，默认是任何空格
 """
 
+# 2、把csv文件读取到Numpy对象中
+"""
+    numpy.loadtxt(fram,dtype=np.float,delimiter=None,unpack=Flase)
+    frame   文件、字符串或产生器，可以是.gz或.bz2的压缩文件
+    dtype   数据类型
+    delimiter   分割字符串，默认是任何空格
+"""
 
+# 3、csv文件的局限性
+"""
+csv只能有效的存取一维、二维数组
+numpy.savetxt()和numpy.loadtxt()只能有效存取一维、二维叔祖
+
+"""
 
 
 
